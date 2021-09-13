4e179f92ed7c624f5e29bd7722bc5eb9710bbc6d
a.zandi <a.zandi@ocd.co.ir>
2021-09-13 18:36:13 +0430

    The first commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,10 +2,6 @@ DROP TABLE IF EXISTS documents;
 CREATE TABLE documents
 (
     id            INT AUTO_INCREMENT PRIMARY KEY,
-    type          VARCHAR(64) NOT NULL,
-    serial_number VARCHAR(64),
-    pages         INT         NOT NULL DEFAULT 0,
-    version       INT         NOT NULL DEFAULT 0,
-    node VARCHAR(100),
-    thread VARCHAR(100)
+    name VARCHAR(64),
+    version       INT         NOT NULL DEFAULT 0
 );
--- src/test/java/com/isolation/level/IsolationLevelApplicationTests.java
+++ src/test/java/com/isolation/level/IsolationLevelApplicationTests.java
@@ -1,5 +1,7 @@
 package com.isolation.level;
 
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,13 +12,13 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @author Ali Karimizandi
  * @since 2021
  */
-@ActiveProfiles("mock")
-@SpringBatchTest
 @SpringBootTest
 @RunWith(SpringRunner.class)
 class IsolationLevelApplicationTests {
 
+    @Test
+    public void contextLoad() {
 
-
+    }
 
 }
